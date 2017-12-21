e4df05e8a685a38e501152268aafbdf81fa1170e
Arkhen <Arkhen@DESKTOP-NRG4CR4>
2017-12-21 18:55:36 +0000

    Primera k2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,21 +1,21 @@
 package kata2;
 import java.util.HashMap; 
 import java.util.Map;
+import java.util.Arrays;
 
 public class Kata2 {
     public static void main(String[] args) {
         int[] array = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
-        Map <Integer,Integer> datos = new HashMap<Integer, Integer>();
-        for (int i = 0; i < array.length; i++) { 
-            if(datos.containsKey(array[i])){
-                datos.put(array[i], datos.get(array[i])+1);
-            }else{
-                datos.put(array[i], 1);
-            }
-        }
-        for (int key : datos.keySet()) {
-            System.out.println(key + "==>" + datos.get(key));
-        }
+        Histogram histo = new Histogram(array);
+       Map<Integer,Integer> histogr = histo.getHistogram();
+       for (int key : histogr.keySet()) {
+           System.out.println(key + "==>" + histogr.get(key));
+       }
+       
+       System.out.println("=============");
+       System.out.println(Arrays.asList(histogr));
+       System.out.println("=============");
+       System.out.println(histogr.entrySet());
     }
     
 }
